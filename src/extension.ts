b05d2c5da3a1bb9a6f93d33b365585e373ff2a21
xiaoyao <15558110117@163.com>
2023-07-23 18:59:30 +0800

    feat: complete statusBar

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,37 @@
 import * as vscode from 'vscode';
+import * as fs from 'node:fs';
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('open-in-github-button2.openProject', () => {
-    vscode.window.showInformationMessage('open project');
-  });
+  let disposable = vscode.commands.registerCommand(
+    'open-in-github-button2.openProject',
+    openProject
+  );
+
+  let statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+
+  statusBar.command = 'open-in-github-button2.openProject';
+  statusBar.tooltip = 'Open in Github';
+  statusBar.text = '$(github-inverted)';
+  statusBar.show();
 
   context.subscriptions.push(disposable);
 }
 
 function openProject() {
-  vscode.extensions.getExtension('xiayaoliu.open-in-github-button2')?.packageJSON;
+  const workspace = `${vscode.workspace.rootPath}/package.json`;
+  const packageBuffer = fs.readFileSync(workspace);
+
+  if (packageBuffer) {
+    const packageJSON = JSON.parse(packageBuffer.toString());
+
+    const url = packageJSON?.repository?.url.replace('git+', '');
+    if (url) {
+      // vscode.window.showInformationMessage(`open project: ${url}`);
+      vscode.env.openExternal(url);
+      return;
+    }
+  }
+  vscode.window.showInformationMessage(`repository.url is not found in package.json`);
 }
 
 export function deactivate() {}
